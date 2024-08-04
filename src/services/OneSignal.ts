88568659b11ad3b0d6a4db609016bbcf31ad157b
ElrohirGT <elrohirgt@gmail.com>
2024-08-04 17:31:14 -0600

    feat: Added OneSignal service

---
+++ src/services/OneSignal.ts
@@ -0,0 +1,53 @@
+import * as OneSignalLib from '@onesignal/node-onesignal';
+
+const ONESIGNAL_APP_ID = process.env.ONESIGNAL_APP_ID as string;
+const ONESIGNAL_REST_API_KEY = process.env.ONESIGNAL_REST_API_KEY as string;
+const ANDROID_URGENT_CHANNEL_ID = 'uuid';
+
+const app_key_provider = {
+  getToken() {
+    return ONESIGNAL_REST_API_KEY;
+  },
+};
+
+const configuration = OneSignalLib.createConfiguration({
+  authMethods: {
+    rest_api_key: {
+      tokenProvider: app_key_provider,
+    },
+  },
+});
+const client = new OneSignalLib.DefaultApi(configuration);
+
+export type NotificationInfo = {
+	title: string,
+	body: string,
+	external_id: string[]
+}
+
+const sendPushNotification = async ({
+  title,
+  body,
+  external_id,
+}: NotificationInfo): Promise<OneSignalLib.NotificationWithMeta> => {
+  const notification = new OneSignalLib.Notification();
+  notification.app_id = ONESIGNAL_APP_ID;
+  notification.include_aliases = { external_id };
+  notification.target_channel = 'push';
+  notification.android_channel_id = ANDROID_URGENT_CHANNEL_ID;
+
+  notification.headings = {
+    en: title,
+  };
+  notification.contents = {
+    en: body,
+  };
+
+  const { id } = await client.createNotification(notification);
+  const response = await client.getNotification(ONESIGNAL_APP_ID, id as string);
+  return response;
+};
+
+export const OneSignal = {
+  sendPushNotification,
+};
