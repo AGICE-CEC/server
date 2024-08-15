2cee764ee98ce0ac929f12cc8ec7ee5f1ff59ded
Albert Johanson <albert@studio31.io>
2024-08-15 15:14:39 -0600

    [ADD] Migration for each table with dummy data

---
+++ src/migrations/20240815205923-add-ubication-table.js
@@ -0,0 +1,31 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('UBICATION', [
+      {
+        locationName: 'Central Park',
+        locationDescription: 'A large public park in New York City.',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        locationName: 'Louvre Museum',
+        locationDescription: 'The world’s largest art museum located in Paris, France.',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        locationName: 'Sydney Opera House',
+        locationDescription: 'A multi-venue performing arts centre in Sydney, Australia.',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('UBICATION', null, {});
+  }
+};
