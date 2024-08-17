88e173458fecda60c137bcd23ed6ff3b1314119f
Albert Johanson <albert@studio31.io>
2024-08-17 13:08:44 -0600

    [FIX] typo in migration for events

--- src/migrations/20240815210237-add-event-table-items.js
+++ src/migrations/20240815210237-add-event-table-items.js
@@ -7,21 +7,21 @@ module.exports = {
       {
         title: 'Concert at Central Park',
         description: 'A large concert at Central Park in New York City.',
-        ubicacionId: 1,  // Assuming this corresponds to an existing ubicacionId in the Ubication table
+        ubicationId: 1,  // Assuming this corresponds to an existing ubicacionId in the Ubication table
         createdAt: new Date(),
         updatedAt: new Date(),
       },
       {
         title: 'Louvre Art Exhibition',
         description: 'A special art exhibition at the Louvre Museum in Paris.',
-        ubicacionId: 2,  // Assuming this corresponds to an existing ubicacionId in the Ubication table
+        ubicationId: 2,  // Assuming this corresponds to an existing ubicacionId in the Ubication table
         createdAt: new Date(),
         updatedAt: new Date(),
       },
       {
         title: 'Sydney Opera House Tour',
         description: 'A guided tour of the Sydney Opera House.',
-        ubicacionId: 3,  // Assuming this corresponds to an existing ubicacionId in the Ubication table
+        ubicationId: 3,  // Assuming this corresponds to an existing ubicacionId in the Ubication table
         createdAt: new Date(),
         updatedAt: new Date(),
       },
