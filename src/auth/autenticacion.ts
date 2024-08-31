5700d2101f7f7396e300a41cd81e8f1cb7013b5b
Diego <lop23747@uvg.edu.gt>
2024-08-31 09:37:22 -0600

    Autenticacion con AuthGoogle terminado y funcional

--- src/auth/autenticacion.ts
+++ src/auth/autenticacion.ts
@@ -1,4 +1,5 @@
 import * as fs from 'fs';
+import { GoogleSpreadsheet, GoogleSpreadsheetRow } from 'google-spreadsheet';
 import * as readline from 'readline';
 import * as dotenv from 'dotenv';
 import { google } from 'googleapis';
@@ -6,14 +7,15 @@ import * as path from 'path'; // Para manejar las rutas correctamente
 
 dotenv.config(); // Cargar variables de entorno desde el archivo .env
 
+
+
 // Variables de entorno
 const SCOPES = ['https://www.googleapis.com/auth/spreadsheets'];
 
-const TOKEN_PATH = process.env.TOKEN_PATH ? path.resolve(process.env.TOKEN_PATH) : null;
-const CRED_PATH = process.env.CRED_PATH ? path.resolve(process.env.CRED_PATH) : null;
+const sheeCredentials = JSON.parse(Buffer.from(process.env.GOOGLE_SHEETS_BASE64 ?? '', 'base64').toString('ascii'));
 const SPREADSHEET_ID = process.env.SPREADSHEET_ID;
 
-if (!TOKEN_PATH || !CRED_PATH || !SPREADSHEET_ID) {
+if (!sheeCredentials || !SPREADSHEET_ID) {
   throw new Error('Faltan variables de entorno necesarias.');
 }
 
@@ -21,98 +23,43 @@ if (!TOKEN_PATH || !CRED_PATH || !SPREADSHEET_ID) {
 // Función para verificar las credenciales de un usuario
 async function checkCredentials(user: string): Promise<boolean> {
   // se obtiene la autorización para acceder a la hoja de cálculo
-  const auth = await authorize(JSON.parse(fs.readFileSync(CRED_PATH ?? '', 'utf8')));
-  const sheets = google.sheets({ version: 'v4', auth });
-
-  // Obtener la primera hoja del documento
-  const doc = await sheets.spreadsheets.get({
-    spreadsheetId: SPREADSHEET_ID
+  const sheetsAuth = new google.auth.GoogleAuth({
+    credentials: sheeCredentials,
+    scopes: SCOPES
   });
-  const sheet = doc.data.sheets ? doc.data.sheets[0] : null;
-  if (!sheet || !sheet.properties || !sheet.properties.title) {
-    throw new Error('No se pudo obtener la primera hoja del documento');
-  }
-  const sheetTitle = sheet.properties.title;
+  
+  
+  const cec_sheets = new GoogleSpreadsheet(String(SPREADSHEET_ID), sheetsAuth)
 
-  // Definir el rango usando el título de la primera hoja
-  const range = `${sheetTitle}!A1:U`;
+  await cec_sheets.loadInfo(); // carga la información de la hoja de cálculo
+  const sheet = cec_sheets.sheetsByIndex[0]; // obtiene la primera hoja del documento
+  
 
-  // Obtener los valores de la hoja
-  const r = await sheets.spreadsheets.values.get({
-    spreadsheetId: SPREADSHEET_ID,
-    range: range
-  });
+  await cec_sheets.loadCells('A1:U'); // carga las celdas de la hoja de cálculo
+  
+  
+    if (!sheet) {
+        throw new Error('No se pudo obtener la primera hoja del documento');
+    }
 
   // se llama la función verifyUser con el nombre de usuario y el objeto sheets
-  const verified = await verifyPayment(user, r.data.values);
+  const verified = await verifyPayment(user, (sheet));
   return verified; // se retorna el resultado
 }
 
 // Verificar si existe el usuario
 async function verifyPayment(username: string, sheets: any): Promise<boolean> {
-  const values = sheets;
-  for (const row of values) { // Se recorre cada fila de la hoja de cálculo
-    if (row[2] === username) { // Verificar si el nombre de usuario coincide
-      return row[17] === "OK";
+    const rows = await sheets.getRows(); // Await the rows from the sheet
+
+    for (const row of rows) { // Iterate over each row
+        if (row.get("Correo electrónico") === username) { // Check if the username matches
+            return row.get("Verificación Qpaypro") === "OK"; // Return true if verification is OK
+        }
     }
-  }
-  return false; // Devolver false si no se encuentra el usuario o no coincide la fecha
+
+    return false; // Return false if no match is found
 }
 
-// Función para autorizar el acceso a la API de Google
-async function authorize(cred: any) {
-  const { client_secret, client_id, redirect_uris } = cred.installed;
-  const oAuth2Client = new google.auth.OAuth2(client_id, client_secret, redirect_uris[0]);
-
-  if (TOKEN_PATH && fs.existsSync(TOKEN_PATH)) {
-    oAuth2Client.setCredentials(JSON.parse(fs.readFileSync(TOKEN_PATH, 'utf8')));
-    return oAuth2Client;
-  }
-
-  return getNewToken<typeof oAuth2Client>(oAuth2Client);
-}
-
-// Función para obtener un nuevo token
-async function getNewToken<T = any>(oAuth2Client: any): Promise<T> {
-  const authUrl = oAuth2Client.generateAuthUrl({
-    access_type: 'offline',
-    scope: SCOPES
-  });
-
-  console.log('Authorize this app by visiting this url:', authUrl);
-  const code = await readlineAsync('Enter the code from that page here: ');
-  const token = await new Promise((resolve, reject) => {
-    oAuth2Client.getToken(code, (err: any, token: any) => {
-      err ? reject(err) : resolve(token);
-    });
-  });
-  oAuth2Client.setCredentials(token);
-
-  // Store the token to disk for later program executions
-  if (TOKEN_PATH) {
-    fs.writeFileSync(TOKEN_PATH, JSON.stringify(token));
-  } else {
-    throw new Error('TOKEN_PATH is undefined');
-  }
-  console.log('Token stored to', TOKEN_PATH);
-
-  return oAuth2Client;
-}
-
-// Función para leer una línea desde la consola
-async function readlineAsync(question: string) {
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-  });
-
-  return new Promise((resolve) => {
-    rl.question(question, (answer) => {
-      rl.close();
-      resolve(answer);
-    });
-  });
-}
 
 // Llamada a checkCredentials
-checkCredentials("dayannavargasmadrigal@gmail.com").then(console.log).catch(console.error);
+//checkCredentials("dayannavargasmadrigal@gmail.com").then(console.log).catch(console.error);
