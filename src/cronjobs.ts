2dd735bdb122017e7d839475c2046c79f9ce2e70
Luisan <luisan@countercheck.com>
2024-09-01 17:54:29 -0600

    event .eventId instead of .id

--- src/cronjobs.ts
+++ src/cronjobs.ts
@@ -1,12 +1,13 @@
 import { CronJob } from 'cron';
 import { Op } from 'sequelize';
 import Event from './models/EVENT';
+import Location from './models/LOCATION';
 import { OneSignal } from './services/OneSignal';
 
 /**
  * Variable to control how many minutes to wait between cronjob invocations.
  */
-const CRONJOB_DELTA_MINS = 10;
+const CRONJOB_DELTA_MINS = 15;
 
 const formatToHour = (date: Date) => {
   return `${date.getHours()}:${date.getMinutes().toString().padStart(2, '0')}`;
@@ -14,20 +15,20 @@ const formatToHour = (date: Date) => {
 
 const alreadyAlertedEvents = new Set();
 
-new CronJob(
-  `*/${CRONJOB_DELTA_MINS} * * * *`, // every 10 minutes
-  async () => {
-    const now = new Date();
-    const future = new Date(now.getTime() + CRONJOB_DELTA_MINS * 60 * 1000);
-    const events = await Event.findAll({
-      where: {
-        eventDay: now.getDate(),
-        hourStart: {
-          [Op.between]: [formatToHour(now), formatToHour(future)],
-        },
-        id: {
-          [Op.notIn]: Array.from(alreadyAlertedEvents),
-        },
+export const alertAttendees = async () => {
+  const now = new Date();
+
+  const future = new Date(now.getTime() + CRONJOB_DELTA_MINS * 60 * 1000);
+  const events = await Event.findAll({
+    where: {
+      eventDay: now.getDate(),
+      hourStart: {
+        [Op.between]: [formatToHour(now), formatToHour(future)],
+      },
+      eventId: {
+        [Op.notIn]: Array.from(alreadyAlertedEvents) as number[],
+      },
+    },
     include: [
       {
         model: Location,
@@ -48,8 +49,12 @@ new CronJob(
     });
   });
 
-    await Promise.all(tasks);
-  }, // onTick
+  await Promise.all(tasks);
+};
+
+new CronJob(
+  `*/${CRONJOB_DELTA_MINS} * * * *`, // every 10 minutes
+  alertAttendees, // onTick
   null, // onComplete
   true, // autostart
   'America/Guatemala' // timeZone
