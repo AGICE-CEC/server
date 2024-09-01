9757ec8e0b25c749ed5e5b48c831f5a6edcb7fdf
ElrohirGT <elrohirgt@gmail.com>
2024-08-31 21:44:56 -0600

    feat: Improved cron logic

--- src/cronjobs.ts
+++ src/cronjobs.ts
@@ -1,17 +1,45 @@
 import { CronJob } from "cron";
 import Event from "./models/EVENT";
 import { OneSignal } from "./services/OneSignal";
+import { Op } from "sequelize";
+
+/**
+ * Variable to control how many minutes to wait between cronjob invocations.
+ */
+const CRONJOB_DELTA_MINS = 10;
+
+/**
+ * Variable to control how many minutes to wait before sending a notification for a meeting.
+ */
+const EVENT_DELTA_MINS = 10;
+
+const formatToHour = (date: Date) => {
+	return `${date.getHours()}:${date.getMinutes()}`;
+};
+
+const alreadyAlertedEvents = new Set();
 
 new CronJob(
-	"*/10 * * * *", // every 10 minutes
+	`*/${CRONJOB_DELTA_MINS} * * * *`, // every 10 minutes
 	async () => {
+		const now = new Date();
+		// We add +1 because BETWEEN is exclusive in sequelize
+		const future = new Date(now.getTime() + (EVENT_DELTA_MINS + 1) * 60 * 1000);
 		const events = await Event.findAll({
 			where: {
-				eventDay: new Date().getDate(),
+				eventDay: now.getDate(),
+				hourStart: {
+					[Op.between]: [formatToHour(now), formatToHour(future)],
+				},
+				id: {
+					[Op.notIn]: Array.from(alreadyAlertedEvents),
+				},
 			},
 		});
 
 		const tasks = events.map((ev) => {
+			alreadyAlertedEvents.add(ev.id);
+
 			OneSignal.sendPushNotification({
 				title: ev.title,
 				body: ev.description,
